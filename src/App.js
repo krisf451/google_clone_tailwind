be49e744ba517cc7277242d55a25c84a16a7e0fc
Kristian Fulkerson <kristianf451@gmail.com>
2022-01-22 20:55:35 -0800

    dark mode working, setting up routes, navbar done, buttons done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
+import React, { useState } from "react";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import MyRoutes from "./components/MyRoutes";
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div className="app">
-      <h2>App</h2>
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
+        <MyRoutes />
+        <Footer />
+      </div>
     </div>
   );
 }
